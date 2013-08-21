f779a635997d3d0ecad6bb7185f726c8610b3f25
Godabiola <Goabiola@gmail.com>
2013-08-21 18:21:41 -0400

    Update NOR_Data_analysis_Ushape.m

--- NOR_Data_analysis_Ushape.m
+++ NOR_Data_analysis_Ushape.m
@@ -1,7 +1,8 @@
-r1=transpose(X1_30_Rs(:,1)+1);
-r2=transpose(X1_30_Rs(:,2)+1);
 
-Q = [columnA columnC columnD columnF columnG columnL columnN]; % [ FrameNumber Headx Heady Tailx Taily Centroidx Centroidy]
+r1=transpose(X3_30_Rs(:,1)+1);
+r2=transpose(X3_30_Rs(:,2)+1);
+
+Q = [columnA columnF columnG columnI columnJ columnL columnN]; % [ FrameNumber Headx Heady Tailx Taily Centroidx Centroidy]
 
 for i = 1:length(r1)
     X = Q(r1(i):r2(i),2:3);
@@ -10,8 +11,8 @@ for i = 1:length(r1)
     Q(r1(i):r2(i),4:5) = X(:,:);
 end
 
-Final_m = [Q(:,2:3), Q(:,6:7), Q(:,2)-Q(:,6), Q(:,3)-Q(:,7)];
-FramNum = transpose(Q(:,1));
+Final_m = [Q(1:7500,2:3), Q(1:7500,6:7), Q(1:7500,2)-Q(1:7500,6), Q(1:7500,3)-Q(1:7500,7)];
+FramNum = transpose(Q(1:7500,1));
 posit_direc=transpose(Final_m); % compiles relevant data in one matrix (frame number ,position, then head direction by row)
 xaxis = [ 1,0];
 %procframnum = zeros(1, length(posit_direc(1,:))); % number of the frame (which comes from modified SOS processing)
@@ -19,7 +20,7 @@ FvB = zeros(1, length(posit_direc(1,:))); % 1 = facing; -1 = back to; 0 = noise/
 Side = zeros(1, length(posit_direc(1,:))); % 1 = left; -1 = right; 0 = midline
 
 for i=1:length(posit_direc(1,:)) % Run through every processed frame 
-%    procframnum(i) = i;
+
     theta = rad2deg(atan2(xaxis(1)*posit_direc(6,i)-posit_direc(5,i)*xaxis(2),xaxis(1)*posit_direc(5,i)+xaxis(2)*posit_direc(6,i))); %calculate angle of head direction relative to pre defined x-axis
 % Facing stimulus
 if theta < 0 
@@ -54,6 +55,14 @@ else %rat's head is on center line
 end
 
 end
+% Check if Head was within 2 centimeter region 
+
+X2cmL = [193 220 220 347 347 337]'; % x-coordinates of 2cm on left
+Y2cmL = [91 145 154 127 119 65]'; % y coordinates of 2cm box on left
+X2cmR = [428 424 424 555 555 584]'; % x coordinates of 2cm on left
+Y2cmR = [62 120 126 145 141 90]'; % y coordinates of 2cm box on right
+inL2cm = inpolygon(Final_m(:,1),Final_m(:,2),X2cmL,Y2cmL); % Check to see if head coordinate is in the 2cm region
+inR2cm = inpolygon(Final_m(:,1),Final_m(:,2),X2cmR,Y2cmR); % Check to see if head coordinate is in the 2cm region
 
 head_direction_data = [FramNum; FvB; Side];
 
@@ -65,6 +74,8 @@ LookN = (sum(FvB == 0)) / 25; % Time lost to noise/ loking at nothing
 OnLeft = (sum(Side == 1)) / 25; % Time spent on left side of box in seconds
 OnRight = (sum(Side == -1)) / 25; % Time spent on right side of box in seconds
 Midline = (sum(Side == 0)) / 25; % Time spent on the midline of box in seconds
+in2cmL = (sum(inL2cm))/25; % Time spent with head in 2cm box on left 
+in2cmR = (sum(inR2cm))/25; % Time spent with head in 2cm box on right 
 TotTime = length(FramNum)/(25);
 
 OnLeft_Looking = 0; 
@@ -87,5 +98,5 @@ OnLeft_Looking = OnLeft_Looking/25; % Time spent looking on left side of box in 
 OnRight_Looking = OnRight_Looking/25; % Time spent looking on right side of box in seconds
 %OnRight_LookingRight = OnRight_LookingRight/25; % Time spent looking right on right side of box in seconds
 
-FillinOutput = [OnLeft, OnLeft_Looking, NaN , OnRight, NaN, OnRight_Looking, NaN, NaN, TotTime]; 
+FillinOutput = [OnLeft, OnLeft_Looking, NaN , OnRight, NaN, OnRight_Looking, NaN, NaN, in2cmL, in2cmR, TotTime]; 
 copy(FillinOutput);
